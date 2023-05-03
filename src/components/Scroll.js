93b12b5726b0f59541cc4f14fcda7af74bd141f1
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-05-03 13:16:48 -0600

    dunno commit

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: 'scroll', border: '5px solid black', height: '50%'}}>
+        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '475px', position: 'static', left: 0, bottom: 0, width: '100%'}}>
             {props.children}
         </div>
     );
