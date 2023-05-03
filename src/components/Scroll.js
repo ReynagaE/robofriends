418e4216cfc0bd21a21f15454e88c5fe284cd2eb
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-05-03 11:33:31 -0600

    first commit

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+//const h = 0.6 * Screen.height;
+
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+        <div style={{overflowY: 'scroll', border: '5px solid black', height: '100hv', position: "static", left: 0, bottom: 0, width: '100%'}}>
             {props.children}
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import App from './containers/App';
 import 'tachyons';
 import { createRoot } from 'react-dom/client';
+import { searchRobots, requestRobots } from './reducers';
 
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}>
+                <App />
+            </Provider>);
